2b5e26ec7828ceb21446ef298fa9a5651d9d4f98
Manga08 <bijuu5445@gmail.com>
2020-04-27 22:23:24 -0500

    [Featured]: The 10 task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * check_cycle - Checks if a singly linked list has a cycle in it.
+ * @list: The struct.
+ * Return: 0 or 1.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *checks = list, temp = list;
+
+	while (checks != NULL && temp != NULL && cheks->next != NULL)
+	{
+		checks = checks->next->next, temp = temp->next;
+		if (checks == temp)
+			return (1);
+	}
+	return (0);
+}
