092223bd2e625781cdda499f35608a75ae00d4da
Manga08 <bijuu5445@gmail.com>
2020-04-28 19:43:16 -0500

    [Featured]: The 6, 7, 8, 9, 10, 11 and 12 task

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+def fizzbuzz():
+    for fb in range(1, 101):
+        if fb % 15 == 0:
+            print("FizzBuzz", end=' ')
+        elif fb % 5 == 0:
+            print("Buzz", end=' ')
+        elif fb % 3 == 0:
+            print("Fizz", end=' ')
+        else:
+            print(fb, end=' ')
