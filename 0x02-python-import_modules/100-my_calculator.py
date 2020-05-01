66f57c27d6cb8485af26f25c88ae0f8cccb29f0e
Manga08 <bijuu5445@gmail.com>
2020-04-30 19:18:05 -0500

    [Fix]: The advanced task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
+from sys import argv, exit
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    from sys import argv, exit
-
-    if len(argv) != 4:
-        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
+    if(len(argv) != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
-        a = int(argv[1])
+        n1 = int(argv[1])
         op = argv[2]
-        b = int(argv[3])
-        result = 0
-
-        if argv[2] is "+":
-            result = add(a, b)
-        elif argv[2] is "-":
-            result = sub(a, b)
-        elif argv[2] is "*":
-            result = mul(a, b)
-        elif argv[2] is "/" and int(argv[3]) != 0:
-            result = div(a, b)
+        n2 = int(argv[3])
+        if(op == "+"):
+            print("{} + {} = {}".format(n1, n2, add(n1, n2)))
+        elif(op == "/"):
+            print("{} / {} = {}".format(n1, n2, div(n1, n2)))
+        elif(op == "-"):
+            print("{} - {} = {}".format(n1, n2, sub(n1, n2)))
+        elif(op == "*"):
+            print("{} * {} = {}".format(n1, n2, mul(n1, n2)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
-            print("{} {} {} = {}".format(a, op, b, result))
