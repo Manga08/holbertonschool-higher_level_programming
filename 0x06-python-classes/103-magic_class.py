23f94e1e9708deda071aa9ca6ab0034b20b669d4
Manga08 <bijuu5445@gmail.com>
2020-05-19 18:04:11 -0500

    [Featured]: The Advanced task

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
+"""Magic Class"""
 import math
 
 
 class MagicClass:
-    """Define a magic class."""
-
+    """A Magic class that define the area and the circunference"""
     def __init__(self, radius=0):
-        """Initialize MagicClass."""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        return self.__radius**2 * math.pi
+        """Return the area"""
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """Return the circum"""
         return 2 * math.pi * self.__radius
