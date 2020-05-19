bb036b7684e4a1901ed6cba04ff128678650099a
Manga08 <bijuu5445@gmail.com>
2020-05-19 10:29:01 -0500

    [Featured]: The 1 task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+class Square:
+    """A Square class that define the size"""
+
+    def __init__(self, size):
+        """Initializes the size"""
+        self.__size = size
