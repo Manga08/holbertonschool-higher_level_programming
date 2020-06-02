2c29fe269d5b863a96fcc7661a895d15605beb3d
Manga08 <bijuu5445@gmail.com>
2020-06-02 17:17:34 -0500

    [Featured]: The 6 task

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Geometry Class."""
+
+
+class BaseGeometry:
+    """Geometry Things."""
+
+    def area(self):
+        """Raise an exception."""
+        raise Exception('area() is not implemented')
