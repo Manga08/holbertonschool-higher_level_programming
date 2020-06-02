440f0b5ace50286245dc4458fe69d44b3b6d4e7c
Manga08 <bijuu5445@gmail.com>
2020-06-02 16:42:10 -0500

    [Featured]: The 3 task

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Is kind function."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if the object is an instance of, or if the object."""
+    return isinstance(obj, a_class)
