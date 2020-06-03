24b77889ecbf8b5c74e8921d395a31cc6d056097
Manga08 <bijuu5445@gmail.com>
2020-06-02 20:55:59 -0500

    [Update]: The 10 task

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module of Square Class."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square Parameters."""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
