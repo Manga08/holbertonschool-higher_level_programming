57369350a94cb058980336e0247de03649e528bf
Manga08 <bijuu5445@gmail.com>
2020-08-08 15:40:50 -0500

    [Featured]: The 5 Task

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script that takes in the name of an argument and lists all cities."""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities JOIN states on " +
+                "cities.state_id = states.id WHERE states.name = %s",
+                (argv[4], ))
+    query_rows = cur.fetchall()
+    print(", ".join(row[0] for row in query_rows))
+    cur.close()
+    db.close()
