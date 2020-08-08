580538617f9a013e6f51afc7e3fcdb17eed92b65
Manga08 <bijuu5445@gmail.com>
2020-08-08 14:56:38 -0500

    [Featured]: The 0 Task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Write a script that lists all states from the database hbtn_0e_0_usa
-"""
+"""Script that lists all states from the database hbtn_0e_0_usa."""
 
 if __name__ == "__main__":
     import MySQLdb
