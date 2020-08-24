9ca2f07932d4031411fd3cb7a6f3a8db0fa1ab91
Manga08 <bijuu5445@gmail.com>
2020-08-24 11:48:08 -0500

    [Featured]: The 1 Task

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Script that takes in a URL, sends arequest to the URL."""
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as url_req:
+        print(url_req.headers.get('X-Request-Id'))
