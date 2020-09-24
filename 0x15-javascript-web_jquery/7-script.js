8b3b6fec162818e595a1fb54a7552753ed7df84f
Manga08 <bijuu5445@gmail.com>
2020-09-24 13:17:05 -0500

    [Featured]: The 7 Task

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,3 +1,5 @@
-$.getJSON('https://swapi.co/api/people/5/?format=json', function (resp) {
+$.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function (
+  resp
+) {
   $('#character').text(resp.name);
 });
